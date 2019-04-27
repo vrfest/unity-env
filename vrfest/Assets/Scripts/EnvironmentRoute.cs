fe048c3ac0c6bae367ffb0acf3bb4ee5ef330ea4
AndrewYinLi <ali@oberlin.edu>
2019-04-27 16:49:55 -0700

    Create TV

---
+++ vrfest/Assets/Scripts/EnvironmentRoute.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Route : MonoBehaviour
+{
+    const float surface_y_end = -83.825f;
+    const float stage_y_end = -7.42f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
